18b0519ad488693e0dc1ee9d126a8e2e38075cfa
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-08-31 14:23:10 -0400

    objects

--- Module1/JS/mapChallege.js
+++ Module1/JS/mapChallege.js
@@ -7,10 +7,10 @@ const addTwo = (num) =>{
     return num +2
 }
 
-const recreateMap = (array, fn) => {
+const recreateMap = (array, callback) => {
     const newArray = [] 
     for (let i = 0; i < array.length; i++){
-        const result = fn(array[i])
+        const result = callback(array[i])
         newArray.push(result)
     }
     return newArray
@@ -19,7 +19,36 @@ const recreateMap = (array, fn) => {
 // ADD CODE HERE
 
 // Uncomment these to check your work!
-console.log(typeof subtractTwo); // should log: 'function'
-console.log(typeof recreateMap); // should log: 'function'
-console.log(recreateMap([3,4,5], subtractTwo)); // should log: [ 1, 2, 3 ]
-console.log(recreateMap([3,4,5], addTwo)); // should log: [ 1, 2, 3 ]
+// console.log(typeof subtractTwo); // should log: 'function'
+// console.log(typeof recreateMap); // should log: 'function'
+// console.log(recreateMap([3,4,5], subtractTwo)); // should log: [ 1, 2, 3 ]
+// console.log(recreateMap([3,4,5], addTwo)); // should log: [ 1, 2, 3 ]
+
+
+//
+function clean(item) {
+    console.log(`I just cleaned a ${item}`)
+}
+  
+const nails = ["rusty nail", "rusty nail", "bent nail", "clean nail"]
+  
+for (let i = 0; i < nails.length; i++) {
+    clean(nails[i])
+}
+  
+
+
+// =========== ForEach Challenge
+
+const recreateForEach = (array, callback) => {
+    // loop over the array
+    // call the callback on each of the element inside the array
+}
+
+let alphabet = '';
+const letters = ['a', 'b', 'c', 'd'];
+
+recreateForEach(letters, function(char) {
+  alphabet += char;
+});
+console.log(alphabet); //prints 'abcd'
---
+++ Module1/JS/objects.js
@@ -0,0 +1,100 @@
+const user = {
+    name: 'Abe',
+    age: 30,
+    isLoggedIn: true,
+    balance: 200
+} 
+
+console.log(typeof user.balance);
+user.balance *= 100
+console.log(typeof user.balance);
+console.log(user);
+
+const car = {
+    color: 'blue',
+    hp: 4000,
+    year: 1989,
+    isElectric: false,
+    doors: '2',
+}
+
+car.engine = 'V20' // setting a new property called engine
+// console.log(car.isElectric);
+// console.log(car.color);
+// console.log(car);
+
+car.engine = 'V28' // updating the property engine
+car.color = 'Hot Pink'
+// console.log(car);
+
+car.isElectric = true
+
+if (car.isElectric){
+    console.log('Yes the car is Electric');
+} else {
+    console.log('Nop you need gas!');
+}
+
+car.year = 2015
+
+if (car.year <= 1960){
+    console.log('Hey thats a classic!');
+} else if (car.year <= 2000){
+    console.log('Hey that car is getting old');
+} else {
+    console.log('Wow nice car!');
+}
+
+const obj = {
+	salutation: 'hi',
+	count:4
+}
+
+for (let i = 0; i < obj.count; i++) {
+	console.log(i);
+}
+
+
+const house = {}
+house.color = 'Red'
+house.doors = 2
+house.floors = 1
+
+console.log(house);
+console.log(house.alarm);
+
+if (house.alarm){
+    console.log('House is secured');
+} else {
+    console.log('You need an alarm system');
+}
+
+const dev = {
+    programmingLanguages: ['javascript', 'python', 'html', 'css'],
+    address: { 
+        street: '123 Main Street',
+        "zip code": '0845849',
+        state: 'NY',
+        city: 'Yonkers'
+    }
+}
+console.log( dev.address.state );
+dev.address.apt = '1A'
+console.log(dev.address["zip code"]);
+
+console.log(dev.programmingLanguages[dev.programmingLanguages.length -1]);
+console.log(dev.programmingLanguages[3]);
+
+const devCity = dev.address.city
+console.log(devCity);
+// dev.programmingLanguages.forEach((language) => console.log(language.toUpperCase()))
+
+const teams = {
+    1: 'Blue Team',
+    2: 'Red Team',
+    3: 'Purple Team'
+}
+
+const teamTwo = 3
+
+console.log(teams[1]);
